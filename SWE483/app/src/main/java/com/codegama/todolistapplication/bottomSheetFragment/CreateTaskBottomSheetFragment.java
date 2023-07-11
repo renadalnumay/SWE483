55a32cfd6f82341906cb12e5a79654ff3a044531
RenadAlnumay <94903216+renadalnumay@users.noreply.github.com>
2023-07-11 17:33:24 +0300

    Add files via upload

---
+++ SWE483/app/src/main/java/com/codegama/todolistapplication/bottomSheetFragment/CreateTaskBottomSheetFragment.java
@@ -0,0 +1,378 @@
+package com.codegama.todolistapplication.bottomSheetFragment;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.codegama.todolistapplication.R;
+import com.codegama.todolistapplication.activity.AlarmActivity;
+import com.codegama.todolistapplication.activity.MainActivity;
+import com.codegama.todolistapplication.broadcastReceiver.AlarmBroadcastReceiver;
+import com.codegama.todolistapplication.database.DatabaseClient;
+import com.codegama.todolistapplication.model.Task;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.zubair.alarmmanager.builder.AlarmBuilder;
+import com.zubair.alarmmanager.enums.AlarmType;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+import static android.content.Context.ALARM_SERVICE;
+
+public class CreateTaskBottomSheetFragment extends BottomSheetDialogFragment {
+    private View contentView;
+    Unbinder unbinder;
+    @BindView(R.id.addTaskTitle)
+    EditText addTaskTitle;
+    @BindView(R.id.nature_spinner)
+    Spinner addTaskDescriptionSpinner;
+    @BindView(R.id.priority_spinner)
+    Spinner addTaskPrioritySpinner;
+
+    @BindView(R.id.period_spinner)
+    Spinner addTaskPeriodSpinner;
+    @BindView(R.id.taskDate)
+    EditText taskDate;
+    @BindView(R.id.taskTime)
+    EditText taskTime;
+    @BindView(R.id.taskEvent)
+    EditText taskEvent;
+    @BindView(R.id.addTask)
+    Button addTask;
+    int taskId;
+    boolean isEdit;
+    Task task;
+    int mYear, mMonth, mDay;
+    int mHour, mMinute;
+    setRefreshListener setRefreshListener;
+    AlarmManager alarmManager;
+    TimePickerDialog timePickerDialog;
+    DatePickerDialog datePickerDialog;
+    MainActivity activity;
+    public static int count = 0;
+
+    private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
+
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+            }
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
+
+
+    public void setTaskId(int taskId, boolean isEdit, setRefreshListener setRefreshListener, MainActivity activity) {
+        this.taskId = taskId;
+        this.isEdit = isEdit;
+        this.activity = activity;
+        this.setRefreshListener = setRefreshListener;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @SuppressLint({"RestrictedApi", "ClickableViewAccessibility"})
+    @Override
+    public void setupDialog(Dialog dialog, int style) {
+        super.setupDialog(dialog, style);
+        View contentView = View.inflate(getContext(), R.layout.fragment_create_task, null);
+        unbinder = ButterKnife.bind(this, contentView);
+        dialog.setContentView(contentView);
+
+        // Initialize the Spinner view using findViewById()
+        Spinner addTaskDescriptionSpinner = contentView.findViewById(R.id.nature_spinner);
+
+        // Create an ArrayAdapter using the options array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.nature, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the Spinner view
+        addTaskDescriptionSpinner.setAdapter(adapter);
+
+        // Initialize the Priority Spinner view using findViewById()
+        Spinner prioritySpinner = contentView.findViewById(R.id.priority_spinner);
+
+// Create an ArrayAdapter using the priority options array and a default spinner layout
+        ArrayAdapter<CharSequence> priorityAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.priority, android.R.layout.simple_spinner_item);
+
+// Specify the layout to use when the list of choices appears
+        priorityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+// Apply the adapter to the Priority Spinner view
+        prioritySpinner.setAdapter(priorityAdapter);
+
+
+        Spinner periodSpinner = contentView.findViewById(R.id.period_spinner);
+
+// Create an ArrayAdapter using the priority options array and a default spinner layout
+        ArrayAdapter<CharSequence> periodAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.period, android.R.layout.simple_spinner_item);
+
+// Specify the layout to use when the list of choices appears
+        periodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+// Apply the adapter to the Priority Spinner view
+        periodSpinner.setAdapter(periodAdapter);
+
+
+
+
+        alarmManager = (AlarmManager) getActivity().getSystemService(ALARM_SERVICE);
+        addTask.setOnClickListener(view -> {
+            if(validateFields())
+                createTask();
+        });
+        if (isEdit) {
+            showTaskFromId();
+        }
+
+        taskDate.setOnTouchListener((view, motionEvent) -> {
+            if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(getActivity(),
+                        (view1, year, monthOfYear, dayOfMonth) -> {
+                            taskDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            datePickerDialog.dismiss();
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePickerDialog.show();
+            }
+            return true;
+        });
+
+        taskTime.setOnTouchListener((view, motionEvent) -> {
+            if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                // Get Current Time
+                final Calendar c = Calendar.getInstance();
+                mHour = c.get(Calendar.HOUR_OF_DAY);
+                mMinute = c.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                timePickerDialog = new TimePickerDialog(getActivity(),
+                        (view12, hourOfDay, minute) -> {
+                            taskTime.setText(hourOfDay + ":" + minute);
+                            timePickerDialog.dismiss();
+                        }, mHour, mMinute, false);
+                timePickerDialog.show();
+            }
+            return true;
+        });
+    }
+
+    public boolean validateFields() {
+        if(addTaskTitle.getText().toString().equalsIgnoreCase("")) {
+            Toast.makeText(activity, "Please enter a valid title", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(addTaskDescriptionSpinner.getSelectedItem().toString().equalsIgnoreCase("")) {
+            Toast.makeText(activity, "Please select a nature", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(taskDate.getText().toString().equalsIgnoreCase("")) {
+            Toast.makeText(activity, "Please enter date", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(taskTime.getText().toString().equalsIgnoreCase("")) {
+            Toast.makeText(activity, "Please enter time", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(taskEvent.getText().toString().equalsIgnoreCase("")) {
+            Toast.makeText(activity, "Please enter an event", Toast.LENGTH_SHORT).show();
+            return false;
+        }else if (addTaskPeriodSpinner.getSelectedItemPosition() == AdapterView.INVALID_POSITION) {
+            Toast.makeText(activity, "Please select a period", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    private void createTask() {
+        class saveTaskInBackend extends AsyncTask<Void, Void, Void> {
+            @SuppressLint("WrongThread")
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Task createTask = new Task();
+                createTask.setTaskTitle(addTaskTitle.getText().toString());
+                createTask.setTaskDescrption(addTaskDescriptionSpinner.getSelectedItem().toString());
+                createTask.setTaskPriority(addTaskPrioritySpinner.getSelectedItem().toString());
+                createTask.setDate(taskDate.getText().toString());
+                createTask.setLastAlarm(taskTime.getText().toString());
+                createTask.setEvent(taskEvent.getText().toString());
+
+                if (!isEdit)
+                    DatabaseClient.getInstance(getActivity()).getAppDatabase()
+                            .dataBaseAction()
+                            .insertDataIntoTaskList(createTask);
+                else
+                    DatabaseClient.getInstance(getActivity()).getAppDatabase()
+                            .dataBaseAction()
+                            .updateAnExistingRow(taskId, addTaskTitle.getText().toString(),
+                                    addTaskDescriptionSpinner.getSelectedItem().toString(),
+                                    taskDate.getText().toString(),
+                                    taskTime.getText().toString(),
+                                    taskEvent.getText().toString());
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    createAnAlarm();
+                }
+                setRefreshListener.refresh();
+                Toast.makeText(getActivity(), "Your event is been added", Toast.LENGTH_SHORT).show();
+                dismiss();
+
+            }
+        }
+        saveTaskInBackend st = new saveTaskInBackend();
+        st.execute();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void createAnAlarm() {
+        try {
+            String[] items1 = taskDate.getText().toString().split("-");
+            String dd = items1[0];
+            String month = items1[1];
+            String year = items1[2];
+
+            String[] itemTime = taskTime.getText().toString().split(":");
+            String hour = itemTime[0];
+            String min = itemTime[1];
+
+            Calendar cur_cal = new GregorianCalendar();
+            cur_cal.setTimeInMillis(System.currentTimeMillis());
+
+            Calendar cal = new GregorianCalendar();
+            cal.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hour));
+            cal.set(Calendar.MINUTE, Integer.parseInt(min));
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            cal.set(Calendar.DATE, Integer.parseInt(dd));
+
+            Intent alarmIntent = new Intent(activity, AlarmBroadcastReceiver.class);
+            alarmIntent.putExtra("TITLE", addTaskTitle.getText().toString());
+            alarmIntent.putExtra("DESC", addTaskDescriptionSpinner.getSelectedItem().toString());
+            alarmIntent.putExtra("DATE", taskDate.getText().toString());
+            alarmIntent.putExtra("TIME", taskTime.getText().toString());
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(activity,count, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+                } else {
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+                }
+                count ++;
+
+                PendingIntent intent = PendingIntent.getBroadcast(activity, count, alarmIntent, 0);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis() - 600000, intent);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis() - 600000, intent);
+                    } else {
+                        alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis() - 600000, intent);
+                    }
+                }
+                count ++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showTaskFromId() {
+        class showTaskFromId extends AsyncTask<Void, Void, Void> {
+            @SuppressLint("WrongThread")
+            @Override
+            protected Void doInBackground(Void... voids) {
+                task = DatabaseClient.getInstance(getActivity()).getAppDatabase()
+                        .dataBaseAction().selectDataFromAnId(taskId);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                setDataInUI();
+            }
+        }
+        showTaskFromId st = new showTaskFromId();
+        st.execute();
+    }
+
+    private void setDataInUI() {
+
+
+            Spinner addTaskDescriptionSpinner = contentView.findViewById(R.id.nature_spinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.nature, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            addTaskDescriptionSpinner.setAdapter(adapter);
+
+
+// Set the selected item in the Spinner to the task's description
+        addTaskDescriptionSpinner.setSelection(adapter.getPosition(task.getTaskDescrption()));
+        addTaskTitle.setText(task.getTaskTitle());
+        taskTime.setText(task.getLastAlarm());
+        taskEvent.setText(task.getEvent());
+
+    }
+
+    public interface setRefreshListener {
+        void refresh();
+    }
+}
---
+++ SWE483/app/src/main/java/com/codegama/todolistapplication/bottomSheetFragment/ViewDetailsActivity.java
@@ -0,0 +1,154 @@
+package com.codegama.todolistapplication.bottomSheetFragment;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.os.Bundle;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.codegama.todolistapplication.R;
+import com.codegama.todolistapplication.activity.AlarmActivity;
+import com.codegama.todolistapplication.activity.MainActivity;
+import com.codegama.todolistapplication.broadcastReceiver.AlarmBroadcastReceiver;
+import com.codegama.todolistapplication.database.DatabaseClient;
+import com.codegama.todolistapplication.model.Task;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.zubair.alarmmanager.builder.AlarmBuilder;
+import com.zubair.alarmmanager.enums.AlarmType;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import static android.content.Context.ALARM_SERVICE;
+public class ViewDetailsActivity extends BottomSheetDialogFragment {
+
+    Unbinder unbinder;
+    @BindView(R.id.addTaskTitle)
+    TextView addTaskTitle;
+    @BindView(R.id.addTaskDescription)
+    TextView addTaskDescription;
+    @BindView(R.id.taskDate)
+    TextView taskDate;
+    @BindView(R.id.taskTime)
+    TextView taskTime;
+    @BindView(R.id.taskEvent)
+    TextView taskEvent;
+    int taskId;
+
+    Task task;
+    int mYear, mMonth, mDay;
+    int mHour, mMinute;
+    CreateTaskBottomSheetFragment.setRefreshListener setRefreshListener;
+    AlarmManager alarmManager;
+    TimePickerDialog timePickerDialog;
+    DatePickerDialog datePickerDialog;
+    MainActivity activity;
+    public static int count = 0;
+
+    private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
+
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+            }
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
+
+    public void setTaskId(int taskId, CreateTaskBottomSheetFragment.setRefreshListener setRefreshListener, MainActivity activity) {
+        this.taskId = taskId;
+        this.activity = activity;
+        this.setRefreshListener = setRefreshListener;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @SuppressLint({"RestrictedApi", "ClickableViewAccessibility"})
+    @Override
+    public void setupDialog(Dialog dialog, int style) {
+        super.setupDialog(dialog, style);
+        View contentView = View.inflate(getContext(), R.layout.activity_view_details, null);
+        unbinder = ButterKnife.bind(this, contentView);
+        dialog.setContentView(contentView);
+        showTaskFromId();
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    private void showTaskFromId() {
+        class showTaskFromId extends AsyncTask<Void, Void, Void> {
+            @SuppressLint("WrongThread")
+            @Override
+            protected Void doInBackground(Void... voids) {
+                task = DatabaseClient.getInstance(getActivity()).getAppDatabase()
+                        .dataBaseAction().selectDataFromAnId(taskId);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                setDataInUI();
+            }
+        }
+        showTaskFromId st = new showTaskFromId();
+        st.execute();
+    }
+
+    private void setDataInUI() {
+        addTaskTitle.setText(task.getTaskTitle());
+        addTaskDescription.setText(task.getTaskDescrption());
+        taskDate.setText(task.getDate());
+        taskTime.setText(task.getLastAlarm());
+        taskEvent.setText(task.getEvent());
+    }
+
+    public interface setRefreshListener {
+        void refresh();
+    }
+
+}
